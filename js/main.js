8c838c9d74ac29b3104fa78f635b5fd0b7a72aa9
Bobby P <abpupeerapitug@yahoo.com>
2017-01-23 21:54:02 -0600

    Fixed the grid by adding zero margins to Figure

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// init Isotope
-var $container = $('.portfolio-items').isotope('layout');
-
-// filteritems on button click
-$('.portfolio-filter').on( 'click', 'button', function(e) {
-    e.preventDefault();
-    var filterValue = $(this).attr('data-filter');
-    $container.isotope({ filter: filterValue});
-});
